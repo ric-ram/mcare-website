e17237ca69d6de7af45111807c89b39c95c72179
Ricram <ricram.log@gmail.com>
2023-08-31 06:02:23 +0100

    feat(cc): Add testimonial card

---
+++ components/testimonialCard.tsx
@@ -0,0 +1,105 @@
+"use client";
+
+import {
+  Flex,
+  Circle,
+  Box,
+  Image,
+  Badge,
+  useColorModeValue,
+  Icon,
+  chakra,
+  Tooltip,
+  Text,
+  Stack,
+} from "@chakra-ui/react";
+import {
+  StarOutlineRounded,
+  StarRounded,
+  StarHalfRounded,
+} from "@mui/icons-material";
+
+const data = {
+  isNew: true,
+  imageURL:
+    "https://images.unsplash.com/photo-1572635196237-14b3f281503f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=4600&q=80",
+  name: "Wayfarer Classic",
+  price: 4.5,
+  rating: 4.2,
+  numReviews: 34,
+};
+
+interface RatingProps {
+  rating: number;
+}
+
+type TestimonialCardProps = {
+  author: string;
+  review: string;
+  rating: number;
+};
+
+function Rating({ rating }: RatingProps) {
+  return (
+    <Box display="flex" alignItems="center" marginLeft={-1}>
+      {Array(5)
+        .fill("")
+        .map((_, i) => {
+          const roundedRating = Math.round(rating * 2) / 2;
+          if (roundedRating - i >= 1) {
+            return <Icon key={i} as={StarRounded} color={"yellow"} ml={1} />;
+          }
+          if (roundedRating - i === 0.5) {
+            return (
+              <Icon key={i} as={StarHalfRounded} color={"yellow"} ml={1} />
+            );
+          }
+          return (
+            <Icon key={i} as={StarOutlineRounded} color={"yellow"} ml={1} />
+          );
+        })}
+    </Box>
+  );
+}
+
+export default function TestimonialCard({
+  author,
+  review,
+  rating,
+}: TestimonialCardProps) {
+  return (
+    <Box
+      bg={"lightBlue.200"}
+      maxW="396px"
+      maxH="340px"
+      rounded="lg"
+      shadow="md"
+      position="relative"
+    >
+      <Stack px={6} py={8} spacing={16}>
+        <Flex
+          justifyContent="space-between"
+          align="flex-start"
+          direction="column"
+          gap={6}
+        >
+          <Rating rating={rating} />
+          <Text>
+            {review}
+          </Text>
+        </Flex>
+        <Flex justifyContent="space-between" alignContent="center">
+          <Box
+            fontSize="2xl"
+            fontWeight="semibold"
+            as="h4"
+            lineHeight="tight"
+            isTruncated
+          >
+            - {author}
+          </Box>
+        </Flex>
+      </Stack>
+    </Box>
+  );
+}
