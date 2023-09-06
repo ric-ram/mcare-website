96d27dcc6c9128514602ff294f09247c096f7e40
Ricram <ricram.log@gmail.com>
2023-09-06 20:51:29 +0100

    feat(pages): Add services section to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,98 @@
 'use client';
 
-import { Heading, Image, Stack, Text, VStack } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Image,
+  ListItem,
+  Stack,
+  Text,
+  UnorderedList,
+  VStack,
+} from '@chakra-ui/react';
+import { useState } from 'react';
 
 type StepCardProps = {
   image: string;
   step: number;
   text: string;
 };
+
+type ComponentProps = {
+  bgColor?: string;
+};
+
+type Service = {
+  label: string;
+  description: string;
+  bullets?: string[];
+  href?: string;
+};
+
+type ServiceButtonProps = {
+  service: Service;
+  activeService: Service;
+  onClick?: React.MouseEventHandler;
+};
+
+type ServiceDescriptionProps = {
+  activeService: Service;
+};
+
+const SERVICES: Service[] = [
+  {
+    label: 'Cardiorrespiratória',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ornare magna in dolor elementum tempor. Curabitur iaculis, libero id pulvinar cursus, orci eros egestas ex, nec porta elit lectus at lorem. Nam ut nulla sed ex consectetur bibendum. Pellentesque dictum leo sodales consequat tincidunt. Sed eu nunc id nunc facilisis vehicula et ac lacus.',
+    bullets: [
+      'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
+      'Aliquam tincidunt mauris eu risus.',
+      'Vestibulum auctor dapibus neque.',
+    ],
+    href: '/marcacao',
+  },
+  {
+    label: 'Drenagem Linfática',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ornare magna in dolor elementum tempor. Curabitur iaculis, libero id pulvinar cursus, orci eros egestas ex, nec porta elit lectus at lorem. Nam ut nulla sed ex consectetur bibendum. Pellentesque dictum leo sodales consequat tincidunt. Sed eu nunc id nunc facilisis vehicula et ac lacus.',
+    href: '/marcacao',
+  },
+  {
+    label: 'Massagem',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ornare magna in dolor elementum tempor. Curabitur iaculis, libero id pulvinar cursus, orci eros egestas ex, nec porta elit lectus at lorem. Nam ut nulla sed ex consectetur bibendum. Pellentesque dictum leo sodales consequat tincidunt. Sed eu nunc id nunc facilisis vehicula et ac lacus.',
+    bullets: [
+      'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
+      'Aliquam tincidunt mauris eu risus.',
+      'Vestibulum auctor dapibus neque.',
+    ],
+    href: '/marcacao',
+  },
+  {
+    label: 'Músculo-esquelético',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ornare magna in dolor elementum tempor. Curabitur iaculis, libero id pulvinar cursus, orci eros egestas ex, nec porta elit lectus at lorem. Nam ut nulla sed ex consectetur bibendum. Pellentesque dictum leo sodales consequat tincidunt. Sed eu nunc id nunc facilisis vehicula et ac lacus.',
+    bullets: [
+      'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
+      'Aliquam tincidunt mauris eu risus.',
+      'Vestibulum auctor dapibus neque.',
+    ],
+    href: '/marcacao',
+  },
+  {
+    label: 'Neurologia',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ornare magna in dolor elementum tempor. Curabitur iaculis, libero id pulvinar cursus, orci eros egestas ex, nec porta elit lectus at lorem. Nam ut nulla sed ex consectetur bibendum. Pellentesque dictum leo sodales consequat tincidunt. Sed eu nunc id nunc facilisis vehicula et ac lacus.',
+    bullets: [
+      'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
+      'Aliquam tincidunt mauris eu risus.',
+      'Vestibulum auctor dapibus neque.',
+    ],
+    href: '/marcacao',
+  },
+];
 
 const StepCard = ({ image, step, text }: StepCardProps) => {
   return (
@@ -22,7 +108,7 @@ const StepCard = ({ image, step, text }: StepCardProps) => {
       <VStack
         spacing={2}
         bg={{ base: 'pastelBlue', lg: 'none' }}
-        py={1}
+        p={1}
         rounded={'md'}
       >
         <Heading as='h4' variant={'header4'}>
@@ -34,13 +120,93 @@ const StepCard = ({ image, step, text }: StepCardProps) => {
   );
 };
 
-export default function Home() {
+const ServiceButton = ({
+  service,
+  activeService,
+  onClick,
+}: ServiceButtonProps) => {
+  return (
+    <Box
+      name={service.label}
+      pl={3}
+      pr={5}
+      py={5}
+      as='button'
+      borderTop={'1px solid'}
+      borderColor={'darkBlue'}
+      width={'full'}
+      fontSize={'24px'}
+      lineHeight={'32px'}
+      textAlign={{ base: 'center', lg: 'left' }}
+      fontWeight={'500'}
+      color={activeService.label === service.label ? 'pastelBlue' : 'darkBlue'}
+      noOfLines={1}
+      bg={activeService.label === service.label && 'darkBlue'}
+      _hover={{
+        lg: {
+          bg: 'darkBlues.600',
+          color: 'pastelBlue',
+        },
+      }}
+      onClick={onClick}
+    >
+      {service.label}
+    </Box>
+  );
+};
+
+const ServiceDescription = ({ activeService }: ServiceDescriptionProps) => {
+  return (
+    <VStack
+      spacing={8}
+      alignItems={'center'}
+      py={8}
+      display={{ base: 'none', lg: 'flex' }}
+    >
+      <Heading as={'h3'} variant={'header3'}>
+        {activeService.label}
+      </Heading>
+      <Flex direction='column' alignItems='flex-start' gap={4}>
+        <Text textAlign={'left'}>{activeService.description}</Text>
+        {activeService.bullets && (
+          <UnorderedList textAlign={'left'}>
+            {activeService.bullets.map((bullet) => (
+              <ListItem>{bullet}</ListItem>
+            ))}
+          </UnorderedList>
+        )}
+      </Flex>
+      <Button
+        as={'a'}
+        display={{ base: 'none', lg: 'inline-flex' }}
+        fontSize={'16px'}
+        fontWeight={'medium'}
+        lineHeight={'24px'}
+        py={'24px'}
+        px={'24px'}
+        ml={8}
+        color={'black'}
+        bg={'lightBlue.200'}
+        href={activeService.href}
+        _hover={{
+          bg: 'lightBlue.300',
+          fontWeight: 600,
+        }}
+      >
+        Saber mais
+      </Button>
+    </VStack>
+  );
+};
+
+const ProcedureSection = ({ bgColor }: ComponentProps) => {
   return (
     <Stack
       py={12}
       px={{ lg: '160px', base: '32px' }}
       alignItems={'center'}
       spacing={{ base: 6, lg: '95px' }}
+      bg={bgColor}
     >
       <Heading as={'h2'} variant={{ base: 'header2', lg: 'header1' }}>
         Procedimento
@@ -87,5 +253,66 @@ export default function Home() {
         />
       </Stack>
     </Stack>
+  );
+};
+
+const ServicesSection = ({ bgColor }: ComponentProps) => {
+  const [activeService, setActiveService] = useState(SERVICES[0]);
+
+  const handleServiceClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+
+    const activeService: HTMLButtonElement = e.currentTarget;
+    const index = SERVICES.findIndex(
+      (service) => service.label === activeService.name,
+    );
+
+    setActiveService(SERVICES[index]);
+  };
+
+  return (
+    <Stack
+      py={12}
+      px={{ lg: '160px', base: '32px' }}
+      alignItems={'center'}
+      spacing={{ base: 6, lg: 8 }}
+      bg={bgColor}
+    >
+      <Heading as={'h2'} variant={{ base: 'header2', lg: 'header1' }}>
+        Serviços
+      </Heading>
+      <Stack
+        direction={{ base: 'column', lg: 'row' }}
+        alignItems={'center'}
+        backgroundImage={{ base: '', lg: '/images/divider.svg' }}
+        backgroundRepeat={'no-repeat'}
+        backgroundSize={'contain'}
+        backgroundPosition={{ md: '45% 50%', xl: '35% 50%' }}
+        py={8}
+        spacing={'100px'}
+        maxWidth={{ md: '700px', xl: '1000px' }}
+        minWidth={'680px'}
+      >
+        <VStack direction={'column'} gap={0} minWidth={'300px'}>
+          {SERVICES.map((service) => (
+            <ServiceButton
+              service={service}
+              onClick={handleServiceClick}
+              activeService={activeService}
+            />
+          ))}
+        </VStack>
+        <ServiceDescription activeService={activeService} />
+      </Stack>
+    </Stack>
+  );
+};
+
+export default function Home() {
+  return (
+    <>
+      <ProcedureSection />
+      <ServicesSection bgColor='pastelBlue' />
+    </>
   );
 }
