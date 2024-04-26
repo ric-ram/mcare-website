3f4b1880c9a340fa36897560bb99dff6535aac6c
Ricram <ricram.log@gmail.com>
2024-04-26 21:50:21 +0100

    Add image to specialy page

--- app/especialidades/[specialtyId]/page.tsx
+++ app/especialidades/[specialtyId]/page.tsx
@@ -8,8 +8,11 @@ import { AREAS, Area } from '@/data/areas';
 import { SPECIALTIES, Specialty } from '@/data/specialties';
 import {
   Container,
+  Flex,
   Heading,
+  Image,
   ListItem,
+  SimpleGrid,
   Stack,
   Text,
   UnorderedList,
@@ -25,10 +28,11 @@ const getSpecialtyAreas = (specialtyId: string) => {
   return AREAS.filter((area: Area) => area.specialtyId === specialtyId);
 };
 
-export default function SpecialtyPage({ params }: SpecialtyProps) {
-  const specialty = getSpecialty(params.specialtyId);
-  const specialtyAreas = getSpecialtyAreas(params.specialtyId);
-
+const SpecialtyHero = (props: {
+  label: string;
+  paragraphs: string[];
+  hasQuestions?: Boolean | false;
+}) => {
   return (
     <>
       <Container maxWidth={'1120px'}>
@@ -123,6 +127,150 @@ export default function SpecialtyPage({ params }: SpecialtyProps) {
           </Stack>
         </Container>
       </Container>
+      <Heading
+        as='h1'
+        variant={'header1'}
+        textAlign={'center'}
+        mt={props.hasQuestions && 16}
+      >
+        {props.label}
+      </Heading>
+      <Stack
+        maxWidth='full'
+        pt={props.hasQuestions && 16}
+        pb={props.hasQuestions && 24}
+        px={props.hasQuestions && { lg: '160px', base: '32px' }}
+        spacing={16}
+      >
+        {props.paragraphs.map((paragraph: string, index: number) => (
+          <Text
+            textAlign={{ base: 'center', md: 'left' }}
+            fontSize={'lg'}
+            key={index}
+          >
+            {paragraph}
+          </Text>
+        ))}
+      </Stack>
+    </>
+  );
+};
+
+export default function SpecialtyPage({ params }: SpecialtyProps) {
+  const specialty = getSpecialty(params.specialtyId);
+  const specialtyAreas = getSpecialtyAreas(params.specialtyId);
+
+  return (
+    <>
+      {!specialty.questions && (
+        <>
+          <Container
+            maxWidth={'full'}
+            pt={'100px'}
+            pb={12}
+            px={{ lg: '160px', base: '32px' }}
+          >
+            <SimpleGrid
+              columns={{ base: 1, md: 2 }}
+              spacing={20}
+              //px={8}
+              alignItems={'center'}
+              templateAreas={{ base: `'first' 'second'`, md: `'first second'` }}
+            >
+              <Flex
+                justifyContent={'flex-end'}
+                gridArea={{ base: 'first', md: 'second' }}
+              >
+                <Image
+                  rounded={'md'}
+                  alt={specialty.label}
+                  src={specialty.image}
+                  objectFit={'cover'}
+                />
+              </Flex>
+              <Stack
+                spacing={8}
+                gridArea={{ base: 'second', md: 'first' }}
+                alignItems={{ base: 'center', md: 'normal' }}
+              >
+                <SpecialtyHero
+                  label={specialty.label}
+                  paragraphs={specialty.descriptionParagraphs}
+                />
+              </Stack>
+            </SimpleGrid>
+          </Container>
+          {specialty.areas && (
+            <InteractiveTable
+              bgColor='pastelBlue'
+              title='Áreas de Actuação'
+              items={specialtyAreas}
+              type='areas'
+            />
+          )}
+          {specialty.howToSteps && (
+            <Stack
+              maxWidth='full'
+              pt={16}
+              pb={24}
+              px={{ lg: '160px', base: '32px' }}
+            >
+              <HowToCard
+                label={specialty.label}
+                howToSteps={specialty.howToSteps}
+              />
+            </Stack>
+          )}
+        </>
+      )}
+      {specialty.questions && (
+        <>
+          <SpecialtyHero
+            label={specialty.label}
+            paragraphs={specialty.descriptionParagraphs}
+            hasQuestions={true}
+          />
+          <Stack maxWidth='full' pb={24} px={{ lg: '160px', base: '32px' }}>
+            {specialty.questions.map((question, index) => (
+              <Stack key={index} maxWidth='full' pb={8}>
+                <Heading as={'h3'} variant={'header3'}>
+                  {question.questionTitle}
+                </Heading>
+                {question.isBullet && (
+                  <UnorderedList>
+                    {question.description.map((text, index) => (
+                      <ListItem key={index}>{text}</ListItem>
+                    ))}
+                  </UnorderedList>
+                )}
+                {!question.isBullet &&
+                  question.description.map((text, index) => (
+                    <Text key={index}>{text}</Text>
+                  ))}
+              </Stack>
+            ))}
+          </Stack>
+        </>
+      )}
+      <Stack
+        pt={12}
+        pb={24}
+        px={{ lg: '160px', base: '32px' }}
+        minHeight={'650px'}
+        width={'full'}
+        overflow={'hidden'}
+        alignItems={'stretch'}
+        spacing={16}
+        bgColor={(specialty.howToSteps || specialty.questions) && 'pastelBlue'}
+      >
+        <Heading as={'h1'} variant={'header1'} textAlign={'center'}>
+          Faça a sua marcação
+        </Heading>
+        {/* <Text textAlign={'center'} fontSize={'18px'} lineHeight={'24px'}>
+          Tem algo a acrescentar? Partilhe connosco a sua opinão.
+        </Text> */}
+        <AppointmentsForm />
+      </Stack>
     </>
   );
 }
