a4dd684f40f62b9c48ab26c11aba1eb6916fabe5
Ricram <ricram.log@gmail.com>
2024-05-11 18:57:27 +0100

    fix container size

--- app/marcacao/page.tsx
+++ app/marcacao/page.tsx
@@ -1,29 +1,31 @@
 'use client';
 
 import { AppointmentsForm } from '@/components/forms';
-import { Heading, Stack } from '@chakra-ui/react';
+import { Container, Heading, Stack } from '@chakra-ui/react';
 
 export default function Appointment() {
   return (
-    <Stack
-      pt={12}
-      pb={24}
-      px={{ lg: '160px', base: '32px' }}
-      position={'relative'}
-      minHeight={'650px'}
-      width={'full'}
-      overflow={'hidden'}
-      alignItems={'stretch'}
-      spacing={16}
-    >
-      <Heading
-        as={'h2'}
-        variant={{ base: 'header2', lg: 'header1' }}
-        textAlign={'center'}
-      >
-        Marcações
-      </Heading>
-      <AppointmentsForm />
-    </Stack>
+    <Container maxWidth={'full'}>
+      <Container maxWidth={'1120px'}>
+        <Stack
+          pt={12}
+          pb={24}
+          position={'relative'}
+          minHeight={'650px'}
+          overflow={'hidden'}
+          alignItems={'stretch'}
+          spacing={16}
+        >
+          <Heading
+            as={'h2'}
+            variant={{ base: 'header2', lg: 'header1' }}
+            textAlign={'center'}
+          >
+            Marcações
+          </Heading>
+          <AppointmentsForm />
+        </Stack>
+      </Container>
+    </Container>
   );
 }
