c4b3aacce99698283fea176654abe4e85d2281a2
Ricram <ricram.log@gmail.com>
2024-07-01 16:01:05 +0100

    Change tone of modal messages to a more formal tone

--- data/modalMessages.ts
+++ data/modalMessages.ts
@@ -11,48 +11,48 @@ export const MODALMESSAGES: Array<ModalMessage> = [
 		type: ModalTypes.Testimonial,
 		success: false,
 		paragraphs: [
-			'Parece que algo ocorreu de errado. Por favor, tenta mais tarde.',
-			'Se o problema continua a verificar-se, entra contacta-nos directamente.'
+			'Parece que algo ocorreu de errado. Por favor, tente mais tarde.',
+			'Se o problema continua a verificar-se, por favor entre em contacto directamente.'
 		]
 	},
 	{
 		type: ModalTypes.Testimonial,
 		success: true,
 		paragraphs: [
-			'O teu testemunho foi enviado com sucesso!',
-			'Agradecemos as tuas palavras. A tua opinião é bastante importante para nós!'
+			'O seu testemunho foi enviado com sucesso!',
+			'Agradecemos as suas palavras. A sua opinião é bastante importante para nós!'
 		]
 	},
 	{
 		type: ModalTypes.Appointment,
 		success: false,
 		paragraphs: [
-			'Parece que algo ocorreu de errado. Por favor, tenta mais tarde.',
-			'Se o problema continua a verificar-se, entra contacta-nos directamente.'
+			'Parece que algo ocorreu de errado. Por favor, tente mais tarde.',
+			'Se o problema continua a verificar-se, por favor entre em contacto directamente.'
 		]
 	},
 	{
 		type: ModalTypes.Appointment,
 		success: true,
 		paragraphs: [
-			'A tua tentativa de marcação foi enviada com sucesso!',
-			'Iremos entrar em contacto contigo o mais cedo possivel para realizármos o agendamento.'
+			'A sua tentativa de marcação foi enviada com sucesso!',
+			'Iremos entrar em contacto consigo o mais cedo possivel para realizármos o agendamento.'
 		]
 	},
 	{
 		type: ModalTypes.Recruit,
 		success: false,
 		paragraphs: [
-			'Parece que algo ocorreu de errado. Por favor, tenta mais tarde.',
-			'Se o problema continua a verificar-se, entra contacta-nos directamente.'
+			'Parece que algo ocorreu de errado. Por favor, tente mais tarde.',
+			'Se o problema continua a verificar-se, por favor entre em contacto directamente.'
 		]
 	},
 	{
 		type: ModalTypes.Recruit,
 		success: true,
 		paragraphs: [
-			'A tua candidatura foi enviada com sucesso!',
-			'Agradecemos a tua vontade de fazer parte da nossa equipa. Iremos entrar em contacto contigo o mais rápido possível.'
+			'A sua candidatura foi enviada com sucesso!',
+			'Agradecemos a sua vontade de fazer parte da nossa equipa. Iremos entrar em contacto consigo o mais rápido possível.'
 		]
 	},
 ]
