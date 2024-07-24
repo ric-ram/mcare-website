167f78622c13cc58c8c00ad35602d1dd201b5f11
Ricram <ricram.log@gmail.com>
2024-07-24 15:10:22 +0100

    Fix error message

--- app/api/email/appointments/route.ts
+++ app/api/email/appointments/route.ts
@@ -40,7 +40,7 @@ export async function POST(request: Request) {
     });
 
     if (error) {
-      return Response.json({ error }, { status: 500 });
+      return NextResponse.json({ error }, { status: 500 });
     }
 
     return NextResponse.json(
